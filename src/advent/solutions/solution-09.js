5d6db866ab7ab4b55a09893050d721d1e624fe8f
andyhall <andy@fenomas.com>
2022-12-09 14:31:59 +0900

    Day 9

---
+++ src/advent/solutions/solution-09.js
@@ -0,0 +1,79 @@
+
+export function part1(input = '') {
+    var h = [0, 0]
+    var t = [0, 0]
+    var hash = {}
+    var addLoc = (loc) => hash[loc.join(',')] = true
+    var moves = {
+        'R': [1, 0],
+        'L': [-1, 0],
+        'U': [0, 1],
+        'D': [0, -1],
+    }
+
+    addLoc(t)
+    input.split('\n').forEach((line, i) => {
+        var [dir, cstr] = line.trim().split(/\s+/)
+        var move = moves[dir]
+        var ct = parseInt(cstr)
+        for (var iter = 0; iter < ct; iter++) {
+            h[0] += move[0]
+            h[1] += move[1]
+            updateChain(h, t)
+            addLoc(t)
+        }
+    })
+    return Object.keys(hash).length
+}
+
+
+
+
+function updateChain(head, tail) {
+    var dx = head[0] - tail[0]
+    var dy = head[1] - tail[1]
+    var dirx = head[0] > tail[0] ? 1 : -1
+    var diry = head[1] > tail[1] ? 1 : -1
+    if (dx === 0 || dy === 0) {
+        if (Math.abs(dx) > 1) tail[0] += dirx
+        if (Math.abs(dy) > 1) tail[1] += diry
+    } else if (Math.abs(dx) > 1 || Math.abs(dy) > 1) {
+        tail[0] += dirx
+        tail[1] += diry
+    }
+}
+
+
+
+
+export function part2(input = '') {
+    var rope = Array.from(Array(10)).map($ => [0, 0])
+    var hash = {}
+    var addLoc = (loc) => hash[loc.join(',')] = true
+    var moves = {
+        'R': [1, 0],
+        'L': [-1, 0],
+        'U': [0, 1],
+        'D': [0, -1],
+    }
+
+    addLoc(rope[0])
+    input.split('\n').forEach((line, i) => {
+        var [dir, cstr] = line.trim().split(/\s+/)
+
+        var move = moves[dir]
+        var ct = parseInt(cstr)
+        for (var iter = 0; iter < ct; iter++) {
+            rope[0][0] += move[0]
+            rope[0][1] += move[1]
+
+            for (var i = 1; i < rope.length; i++) {
+                updateChain(rope[i - 1], rope[i])
+            }
+            addLoc(rope[rope.length - 1])
+        }
+    })
+    return Object.keys(hash).length
+}
+
+
